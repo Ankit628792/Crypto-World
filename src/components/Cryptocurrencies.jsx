f5eca673329b2bde5b18f3d018dd2aa3e6fc6eeb
Ankit <ankit628792@gmail.com>
2022-02-23 11:22:36 +0530

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -29,9 +29,9 @@ function Cryptocurrencies({ simplified }) {
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
             <Link to={`/crypto/${currency.uuid}`}>
               <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl} />} hoverable>
-                <p>Price: {millify(currency.price)}</p>
-                <p>Market Cap: {millify(currency.marketCap)}</p>
-                <p>Daily change: {millify(currency.change)}</p>
+                <p>Price: ${millify(currency.price)}</p>
+                <p>Market Cap: ${millify(currency.marketCap)}</p>
+                <p>Daily change: {millify(currency.change)}%</p>
               </Card>
             </Link>
           </Col>
